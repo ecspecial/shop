fec0fcb8009dcfd637219477e3cf4ccbb8faddd8
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-08-11 08:33:23 +0300

    Added slider

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import HomeHeader from './HomeHeader';
 import './HomePage.css';
+import { cardData } from './Header/cardSpec';
+import Slider from './Slider/Slider';
 
 const HomePage: React.FC = () => {
     return (
         <div className='home-container'>
-            <HomeHeader />
+            <div className='home-header'>
+                <Slider cardData={cardData} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default HomePage;
--- src/components/Pages/HomePage/Slider/Slider.tsx
+++ src/components/Pages/HomePage/Slider/Slider.tsx
@@ -1,42 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
-import '../HomePage.css';
-import { cardData } from '../HeaderCard/cardSpec';
-import HeaderCard from '../HeaderCard/HeaderCard';
+import { CardSpec } from "../Header/cardSpec";
+import HeaderCard from '../Header/HeaderCard';
+import './Slider.css';
 
 interface SliderProps {
-    children: React.ReactNode[];
+    cardData: CardSpec[];
 }
 
-const Slider: React.FC<SliderProps> = ({children}) => {
-
+const Slider: React.FC<SliderProps> = ({ cardData }) => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [startX, setStartX] = useState<number | null>(null);
+    const sliderRef = useRef<HTMLDivElement | null>(null);
+    const activeSlideRef = useRef<HTMLDivElement | null>(null);
     const [shouldAutoSlide, setShouldAutoSlide] = useState(true);
     const timeoutRef = useRef<number | null>(null);
-    const [startX, setStartX] = useState(0);
+    const [slideDirection, setSlideDirection] = useState(1);
 
     const handlePointerDown = (event: React.PointerEvent<HTMLDivElement>) => {
         setStartX(event.clientX);
     };
 
     const handlePointerUp = (event: React.PointerEvent<HTMLDivElement>) => {
-        handleSwipe(event.clientX);
+        if (startX !== null) {
+            handleSwipe(event.clientX);
+        }
     };
 
     const handleSwipe = (endX: number) => {
-        if (startX < endX) {
-          setActiveIndex(activeIndex === 0 ? children.length - 1 : activeIndex - 1);
+        if (startX! < endX) {
+            if (activeIndex !== 0) {
+                setActiveIndex(activeIndex - 1);
+            }
         } else {
-          setActiveIndex((activeIndex + 1) % children.length);
+            if (activeIndex !== cardData.length -1) {
+                setActiveIndex(activeIndex + 1);
+            }
         }
         setShouldAutoSlide(false);
         setTimeout(() => setShouldAutoSlide(true), 20000);
     };
 
-
     const startSlideShow = () => {
-        timeoutRef.current = window.setInterval( () => {
-            setActiveIndex((prev) => (prev + 1) % children.length)
-        }, 5000);
+        timeoutRef.current = window.setInterval(() => {
+            if (activeIndex === cardData.length - 1) {
+                setSlideDirection(-1);
+            } else if (activeIndex === 0) {
+                setSlideDirection(1);
+            }
+    
+            setActiveIndex((prev) => {
+                const newIndex = prev + slideDirection;
+                if (newIndex < 0) {
+                    return cardData.length - 2;
+                } else if (newIndex === cardData.length) {
+                    return 1;
+                }
+                return newIndex;
+            });
+        }, 10000);
     };
 
     const stopSlideShow = () => {
@@ -56,53 +77,43 @@ const Slider: React.FC<SliderProps> = ({children}) => {
         return () => stopSlideShow();
     }, [activeIndex, shouldAutoSlide]);
 
+    useEffect(() => {
+        if (sliderRef.current && activeSlideRef.current) {
+            const offsetPercentage = -activeIndex * 33.33;
+            const slidesContainer = sliderRef.current.querySelector('.slidesContainer') as HTMLElement;
+            slidesContainer.style.transform = `translateX(${offsetPercentage}%)`;
+            
+            const height = activeSlideRef.current.offsetHeight + 20;
+            sliderRef.current.style.height = `${height}px`;
+        }
+    }, [activeIndex]);
+
     return (
-        <div 
-            className='slider'
-        >
-            {children.map((child, index) => (
-                <div
-                    key={index}
-                    className={`slide ${index === activeIndex ? "active" : ""}`}
-                >
-                    {child}
-                </div>
-            ))}
+        <div className="slider" ref={sliderRef} onPointerDown={handlePointerDown} onPointerUp={handlePointerUp}>
+            <div className="slidesContainer">
+                {cardData.map((spec, index) => (
+                    <div 
+                        key={index} 
+                        className={`slide ${index === activeIndex ? "active" : ""}`}
+                        ref={index === activeIndex ? activeSlideRef : null}
+                    >
+                        <HeaderCard spec={spec}/>
+                    </div>
+                ))}
+            </div>
             <div className="dots">
-                {children.map((_, index) => (
+                {cardData.map((_, index) => (
                     <span
                         key={index}
                         onClick={() => {
                             setActiveIndex(index);
-                            setShouldAutoSlide(false);
-                            setTimeout(() => setShouldAutoSlide(true), 20000)
                         }}
                         className={`dot ${index === activeIndex ? "active" : ""}`}
                     />
                 ))}
             </div>
-            <button
-        className="arrow left"
-        onClick={() => {
-          setActiveIndex(activeIndex === 0 ? children.length - 1 : activeIndex - 1);
-          setShouldAutoSlide(false);
-          setTimeout(() => setShouldAutoSlide(true), 20000);
-        }}
-      >
-        Left
-      </button>
-      <button
-        className="arrow right"
-        onClick={() => {
-          setActiveIndex((activeIndex + 1) % children.length);
-          setShouldAutoSlide(false);
-          setTimeout(() => setShouldAutoSlide(true), 20000);
-        }}
-      >
-        Right
-      </button>
-        </div>
-    )
+        </div> 
+    );
 }
 
 export default Slider;
